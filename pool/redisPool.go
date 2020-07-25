60f4bfb745b84968fef48b1cd93422fe73b2adf8
max <msinev@gmail.com>
2020-07-25 22:38:27 +0300

    replicator/pool package of REDIS pool

---
+++ pool/redisPool.go
@@ -0,0 +1,66 @@
+package pool
+
+import (
+	"github.com/garyburd/redigo/redis"
+	"log"
+	"os"
+	"time"
+)
+
+func Map(vs []string, f func(string) string) []string {
+	vsm := make([]string, len(vs))
+	for i, v := range vs {
+		vsm[i] = f(v)
+	}
+	return vsm
+}
+
+// Factory is a function to create new connections.
+type RedisOperation func(redis.Conn) error
+
+var ExecutorGo = make(chan RedisOperation)
+
+func GetEnvDefault(env string, def string) string {
+	val := os.Getenv(env)
+	if val != "" {
+		println("Returning actual \"" + val + "\" for " + env)
+		return val
+	}
+	println("Returning default \"" + def + "\" for " + env)
+	return def
+}
+
+//rate :=
+
+var redisFuseThrottle = time.Tick(time.Second * 2)
+
+func RedisExecutor(URL string) {
+	defer panic("Executor crashed") // TODO: Remove before flight! Only for DEBUG !!!
+	<-redisFuseThrottle
+	c, err := redis.Dial("tcp", URL)
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	log.Println("Connected to REDIS")
+
+	defer c.Close()
+
+	for {
+		foo, ok := <-ExecutorGo
+		if !ok {
+			log.Println("Channel closed")
+			return
+		}
+
+		err = foo(c)
+		if err != nil {
+			log.Fatal(err)
+			go RedisExecutor(URL)
+			log.Println(err)
+			return
+		}
+	}
+}
